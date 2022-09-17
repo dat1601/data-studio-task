f01d3535672dfa8e169521c5009a6a1fd766406f
dathuynh <pdathuynh@gmail.com>
2022-09-17 06:45:20 -0700

    feat: remove hardcoded payload in registerToken

--- src/registerToken.ts
+++ src/registerToken.ts
@@ -1,27 +1,31 @@
 import HttpMethod = GoogleAppsScript.URL_Fetch.HttpMethod;
 
-const registerToken = (): string => {
+const registerToken = (
+    name: string,
+    email: string,
+    clientId: string
+): string => {
     const payload = {
-        client_id: "ju16a6m81mhid5ue1z3v2g0uh",
-        email: "johndoe@testmail.com",
-        name: "John Doe"
-    }
+        client_id: clientId,
+        email,
+        name,
+    };
     const options = {
-        method: "post" as HttpMethod,
-        payload
-    }
-    const REGISTER_TOKEN_URL = "https://api.supermetrics.com/assignment/register"
+        method: 'post' as HttpMethod,
+        payload,
+    };
+    const REGISTER_TOKEN_URL =
+        'https://api.supermetrics.com/assignment/register';
     try {
-        const response = UrlFetchApp.fetch(REGISTER_TOKEN_URL, options)
-        const {data} = JSON.parse(response.getContentText())
+        const response = UrlFetchApp.fetch(REGISTER_TOKEN_URL, options);
+        const { data } = JSON.parse(response.getContentText());
         if (data && data.sl_token) {
-            return data.sl_token
+            return data.sl_token;
         } else {
-            throw new Error("No sl_token found")
+            throw new Error('No sl_token found');
         }
+    } catch (err) {
+        console.log('Error registering token', err);
+        return '';
     }
-    catch (err) {
-        console.log("Error registering token", err)
-        return ""
-    }
-}
+};
